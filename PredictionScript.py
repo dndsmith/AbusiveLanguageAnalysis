5e38cbbc99da80108fd6d39a415aab4cbdaf7419
Daniel Smith <dnls1297@gmail.com>
2019-10-26 19:48:51 -0400

    Make plot of probabilites

--- PredictionScript.py
+++ PredictionScript.py
@@ -3,16 +3,37 @@ import pandas as pd
 import numpy as np
 import sklearn
 import fasttext
+import matplotlib.pyplot as plt
+import re
+import string
 
-ft = fasttext.load_model('./saved_models/cleaned_data.bin')
-model = pickle.load(open('./saved_models/cleaned_data_pickled.sav', 'rb'))
+def PredictAbuse(text):
+    ft = fasttext.load_model('./saved_models/cleaned_data.bin')
+    model = pickle.load(open('./saved_models/cleaned_data_pickled.sav', 'rb'))
 
-test = ft.get_sentence_vector('I love sarcasm and tearing down other people. Gosh, you are an idiot!')
-test = test.reshape(1, -1)
+    colors = ['green', 'red']
+    labels = ['Not abusive', 'Abusive']
+    xAxis = ['Not abusive', 'Abusive']
 
-abuse = 'Abusive' if model.predict(test)[0] == 1 else 'Not abusive'
-proba = model.predict_proba(test)
-probabilities = 'Not abusive probability estimate: ' + str(proba[0][0]) + '\nAbusive probability estimate: ' + str(proba[0][1])
+    test = ft.get_sentence_vector(text)
+    test = test.reshape(1, -1)
 
-print('Prediction: ' + abuse)
-print(probabilities)
+    abuse = 'Abusive' if model.predict(test)[0] == 1 else 'Not abusive'
+    proba = model.predict_proba(test)
+    probabilities = 'Not abusive probability estimate: ' + str(proba[0][0]) + '\nAbusive probability estimate: ' + str(proba[0][1])
+
+    yAxis = proba[0]
+    plt.bar(xAxis, yAxis, color=colors)
+    plt.title('Probability estimates', fontsize=14)
+    plt.xlabel('Language', fontsize=14)
+    plt.ylabel('Probability', fontsize=14)
+    regex = r'[' + string.punctuation + ']'
+    text = re.sub(regex, '', text)
+    text = re.sub(r' ', '', text)
+    plt.savefig('./img/' + text + '.png')
+
+    print('Prediction: ' + abuse)
+    print(probabilities)
+    return 'Prediction: ' + abuse + '\n' + probabilities
+
+PredictAbuse('I\'m sorry, but that is not correct, because according to the encyclopedia of Oxford, you are wrong.')
