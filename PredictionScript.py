4927ff7f329724881802d518e6c0065d8bbbf060
Daniel Smith <dnls1297@gmail.com>
2019-10-26 19:07:44 -0400

    MVP of Prediction Script

--- PredictionScript.py
+++ PredictionScript.py
@@ -4,14 +4,15 @@ import numpy as np
 import sklearn
 import fasttext
 
-ft = fasttext.load_model('./saved_models/cleaned_data.csv')
+ft = fasttext.load_model('./saved_models/cleaned_data.bin')
 model = pickle.load(open('./saved_models/cleaned_data_pickled.sav', 'rb'))
 
 test = ft.get_sentence_vector('I love sarcasm and tearing down other people. Gosh, you are an idiot!')
+test = test.reshape(1, -1)
 
-print('Prediction:')
-print(model.predict(test))
-print('Probability:')
-print(model.predict_proba(test))
-print('Confidence score:')
-print(model.decision_function(test))
+abuse = 'Abusive' if model.predict(test)[0] == 1 else 'Not abusive'
+proba = model.predict_proba(test)
+probabilities = 'Not abusive probability estimate: ' + str(proba[0][0]) + '\nAbusive probability estimate: ' + str(proba[0][1])
+
+print('Prediction: ' + abuse)
+print(probabilities)
