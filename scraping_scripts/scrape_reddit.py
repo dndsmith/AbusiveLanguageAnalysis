d7ea5d4857556a0c23aeb8379819a29f09a9e64e
Daniel Smith <dnls1297@gmail.com>
2019-12-18 08:48:25 -0500

    Able to get the text from reddit posts now

--- scraping_scripts/scrape_reddit.py
+++ scraping_scripts/scrape_reddit.py
@@ -2,24 +2,30 @@ from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException  
 from selenium.webdriver.common.keys import Keys
 from bs4 import BeautifulSoup
-#import requests
 import csv
-#import time ### check if this is actually needed
 
-# make into command line args
-url = "https://www.reddit.com/r/abuse"
+REDDIT_URL = 'https://www.reddit.com'
 
-browser = webdriver.Firefox()
-browser.get(url)
-html_source = browser.page_source
-browser.quit()
+def scrape_html(url):
+    browser = webdriver.Firefox()
+    browser.get(url)
+    html_source = browser.page_source
+    browser.quit()
 
-subredditSoup = BeautifulSoup(html_source, 'html.parser')
+    return html_source
 
-posts = subredditSoup.find_all("a", {"data-click-id": "body"})
+# FIXME: get urls from command line args
+html = scrape_html(REDDIT_URL + "/r/abuse")
+subreddit_soup = BeautifulSoup(html, 'html.parser')
+post_urls = subreddit_soup.find_all("a", {"data-click-id": "body"})
 
-i=0
-for post in posts:
-    print(i)
-    print(post)
-    i += 1
+for post_url in post_urls:
+    html = scrape_html(REDDIT_URL + post_url["href"])
+    post_soup = BeautifulSoup(html, 'html.parser')
+
+    post_text = post_soup.find_all("div", {"data-click-id": "text"})
+    post_msg = ""
+    for elem in post_text:
+        for s in elem.stripped_strings:
+            post_msg += s
+    print(post_msg)
